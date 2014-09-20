8e98afe2b18ce8b978579404034a86172f179b64
Jiahao Liu <jliu@astrata.eu>
2014-09-20 20:14:58 +0200

    Created the login activity. Created the layout for the login

--- app/src/main/java/com/jiahaoliuliu/storyteller/LoginActivity.java
+++ app/src/main/java/com/jiahaoliuliu/storyteller/LoginActivity.java
@@ -3,12 +3,11 @@ package com.jiahaoliuliu.storyteller;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
-
-public class MainActivity extends ActionBarActivity {
+public class LoginActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
