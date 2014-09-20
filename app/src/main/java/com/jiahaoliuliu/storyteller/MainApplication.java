813cc1957a64e295253952785502e382482f3b41
Jiahao Liu <jliu@astrata.eu>
2014-09-20 20:28:14 +0200

    Created the facebook login

--- app/src/main/java/com/jiahaoliuliu/storyteller/MainApplication.java
+++ app/src/main/java/com/jiahaoliuliu/storyteller/MainApplication.java
@@ -3,6 +3,7 @@ package com.jiahaoliuliu.storyteller;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseFacebookUtils;
 
 /**
  * Created by jliu on 20/09/14.
@@ -13,5 +14,7 @@ public class MainApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Parse.initialize(this, "mUlUK6fANWd1MIw6JvpUQ33C2lEY0fOJtrh9e7lO", "bW0Englla8GwE6VxvXlkq1oC3xBSSk8rsLsvwRPf");
+
+        ParseFacebookUtils.initialize(getString(R.string.facebook_app_id));
     }
 }
