864f737aadd69a56d13524f2b20d4e209487c58e
Jiahao Liu <jliu@astrata.eu>
2014-09-21 09:21:44 +0200

    Created the onClickListener on the Main activity

--- app/src/main/java/com/jiahaoliuliu/storyteller/MainActivity.java
+++ app/src/main/java/com/jiahaoliuliu/storyteller/MainActivity.java
@@ -1,6 +1,5 @@
 package com.jiahaoliuliu.storyteller;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,12 +16,19 @@ public class MainActivity extends BaseSessionActivity {
         setContentView(R.layout.activity_main);
 
         mFacebookLogoutButton = (Button)findViewById(R.id.facebook_logout_button);
-        mFacebookLogoutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mSession.closeAndClearTokenInformation();
-                backToLoginActivity();
-            }
-        });
+        mFacebookLogoutButton.setOnClickListener(onclickListener);
     }
+
+    private View.OnClickListener onclickListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.facebook_logout_button:
+                    mSession.closeAndClearTokenInformation();
+                    backToLoginActivity();
+                    break;
+            }
+        }
+    };
 }
