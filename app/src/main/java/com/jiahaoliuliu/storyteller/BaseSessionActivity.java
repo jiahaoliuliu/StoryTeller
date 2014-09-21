d987c2e05936a87b6f568e7a4ba03d35cc12d264
Jiahao Liu <jliu@astrata.eu>
2014-09-21 09:16:58 +0200

    Created the base session activity which the main activity extends

---
+++ app/src/main/java/com/jiahaoliuliu/storyteller/BaseSessionActivity.java
@@ -0,0 +1,41 @@
+package com.jiahaoliuliu.storyteller;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+
+import com.facebook.Session;
+import com.parse.ParseFacebookUtils;
+
+
+public class BaseSessionActivity extends ActionBarActivity {
+
+    private static final String TAG = "BaseSessionActivity";
+
+    Session mSession;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // First of all check if the user has logged in
+        mSession = ParseFacebookUtils.getSession();
+        if (mSession == null || !mSession.isOpened()) {
+            // There is some problem and the session of the user is not available
+            // Go back to the login screen
+            Log.e(TAG, "The user is in the main activity but the session has not opened. Going back to the login screen");
+            backToLoginActivity();
+        }
+
+    }
+
+    void backToLoginActivity() {
+        Intent backToLoginActivityIntent = new Intent(BaseSessionActivity.this, LoginActivity.class);
+        // Because we are going back to the login activity, the back stack must be clean
+        backToLoginActivityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(backToLoginActivityIntent);
+        finish();
+    }
+}
--- app/src/main/java/com/jiahaoliuliu/storyteller/MainActivity.java
+++ app/src/main/java/com/jiahaoliuliu/storyteller/MainActivity.java
@@ -2,35 +2,19 @@ package com.jiahaoliuliu.storyteller;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import com.facebook.Session;
-import com.parse.ParseFacebookUtils;
 
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends BaseSessionActivity {
 
     private static final String TAG = "MainActivity";
     private Button mFacebookLogoutButton;
-
-    private Session mSession;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // First of all check if the user has logged in
-        mSession = ParseFacebookUtils.getSession();
-        if (mSession == null || !mSession.isOpened()) {
-            // There is some problem and the session of the user is not available
-            // Go back to the login screen
-            Log.e(TAG, "The user is in the main activity but the session has not opened. Going back to the login screen");
-            backToLoginActivity();
-        }
 
         mFacebookLogoutButton = (Button)findViewById(R.id.facebook_logout_button);
         mFacebookLogoutButton.setOnClickListener(new View.OnClickListener() {
@@ -40,11 +24,5 @@ public class MainActivity extends ActionBarActivity {
                 backToLoginActivity();
             }
         });
-    }
-
-    private void backToLoginActivity() {
-        Intent backToLoginActivityIntent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(backToLoginActivityIntent);
-        finish();
     }
 }
